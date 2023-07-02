277893884a3c06eb4bcfff01e93a679890087f55
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-07-01 21:07:22 -0300

    resgistro
    criado a primeira rota junto com seu diretório de funções ;

---
+++ src/controllers/salao/registro.js
@@ -0,0 +1,9 @@
+//imprtar connecção com o banco de dados;
+module.exports = {
+    // função responsável por regitrar os salões;
+    async Registrar(request, response){
+        
+        return console.log('registrar');
+    },
+
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
+const RegistrarSalao = require('./controllers/salao/registro'); //importando funções de registro
 
+//rota de registro de salão;
+routes.get('/registrarsalao', RegistrarSalao.Registrar);
 module.exports = routes;
