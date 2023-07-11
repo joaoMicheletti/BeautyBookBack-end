a2462d580713c7cdbc33df1c80329c0db1dc046a
João Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2023-07-10 23:51:55 -0300

    regra de negócio
    para o salão que encontra-se com o seu plano inativo seu acesso ao sistema será limitado somente a pagina de planos  e seus funcionários não terão acesso ao sistema

--- src/controllers/salao/login.js
+++ src/controllers/salao/login.js
@@ -20,19 +20,35 @@ module.exports = {
                 if(senha_funcionario.length === 0){ // si for (0) não achamos a senha no database; 
                     return response.json('erro no login.');
                 } else { // si chegamos aqui passou en todas as verificações como funcionário;
-                    const Data = {
-                        cpf_funcionario: funcionario[0].cpf_funcionario
+                    //verificar à assinatura do salão en que o funcionário encontra-se cadastrado;
+                    const salao =  await connect('funcionarios').where('cpf_funcionario', cpf_salao).select('cpf_salao');
+                    var cpfsalao = salao[0].cpf_salao;
+                    const status = await connect('salao').where('cpf_salao', cpfsalao).select('assinatura_status');
+
+                    if(status[0].assinatura_status === 'on'){
+                        const Data = {
+                            cpf_funcionario: funcionario[0].cpf_funcionario,
+                        };
+                        //retorne um objeto com a propriedade cpf_fucionario;
+                        return response.json(Data);
+                    } else {
+                        //para o salão que encontra-se com o plano inativo os funncionários não terao acesso ao sistema;
+                        return response.json('Acesso Negado!')
                     }
-                    //retorne um objeto com a propriedade cpf_fucionario;
-                    return response.json(Data);
+                    
                 };
             };
         } else if(cSenha[0].senha != senha ){
             return response.json('erro no login');
         } else { // si chegou aqui passou en todas as verificações como salão;
             //retorne um objeto com a propriedade cpf_salao;
+            // retorne o status do plano, aregra de negócio só  deixara o salão ter acesso a pagina de planos.
+            const status = await connect('salao').where('cpf_salao', cpf_salao).select('assinatura_status');
+            console.log(status)
+            var assinatura_status = status[0].assinatura_status;
             const Data = {
-                cpf_salao
+                cpf_salao,
+                assinatura_status
             };
             return response.json(Data);
         }
