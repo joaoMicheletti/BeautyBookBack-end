30ecb94adcf57e97c2327999ae4370e6afafee97
Micheletti <jv604014@gmail.com>
2023-11-17 00:18:19 -0300

    finalizadas as funcionalidades de busca na agenda

--- src/controllers/salao/buscarnaagenda.js
+++ src/controllers/salao/buscarnaagenda.js
@@ -4,12 +4,20 @@ module.exports = {
         const {dia, mes, ano, cpf_salao} = request.body;
         const agendados = await connect('agenda').where('cpf_salao', cpf_salao    ).where('dia', dia)
         .where('mes', mes).where('ano', ano).select('*');
-        return response.json(agendados);
+        if(agendados.length === 0){
+            return response.json("nenhum agendamento encontrado.");
+        } else {
+            return response.json(agendados);
+        };
     },
     async BuscarFuncionario(request, response){
         const {dia, mes, ano, cpf_funcionario} = request.body;
-        const servicosFinalizados = await connect('agenda').where('cpf_funcionario', cpf_funcionario).where('dia', dia)
+        const agendados = await connect('agenda').where('cpf_funcionario', cpf_funcionario).where('dia', dia)
         .where('mes', mes).where('ano', ano).select('*');
-        return response.json(servicosFinalizados);
+        if(agendados.length === 0){
+            return response.json("nenhum agendamento encontrado.");
+        } else {
+            return response.json(agendados);
+        };
     },
 }
